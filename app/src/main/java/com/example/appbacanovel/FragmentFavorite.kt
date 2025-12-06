3e936d76b6f6053c37977065490348a22e7bafb9
estindwi <estindwi11@gmail.com>
2025-12-06 15:48:50 +0700

    fragmentFavorite.kt masih insyallah benar mohon dikoreksi teman teman

--- app/src/main/java/com/example/appbacanovel/FragmentFavorite.kt
+++ app/src/main/java/com/example/appbacanovel/FragmentFavorite.kt
@@ -1,17 +1,37 @@
 package com.example.appbacanovel
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 
 class FragmentFavorite : Fragment() {
+
+    private lateinit var favorite_recycler_view : RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        val view = inflater.inflate(R.layout.fragment_favorite, container, false)
+
+        favorite_recycler_view = view.findViewById(R.id.rv_favorite)
+
+        favorite_recycler_view.layoutManager =
+            androidx.recyclerview.widget.LinearLayoutManager(requireContext())
+
+        favorite_recycler_view.adapter = NovelAdapter(
+            itemList = BookData.getBookList(),
+            mode = NovelAdapter.NovelMode.favorite_page
+        ){ selectedBook ->
+            val intent = Intent ( requireContext(), BookDetailsActivity::class.java)
+            intent.putExtra("book.id", selectedBook.id)
+            startActivity(intent)
+        }
+        return view
     }
 }
